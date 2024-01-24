3481805510c7356ef12cd4ae55f92fe393e0e201
❤️ <shah@priansh.com>
2024-01-24 16:42:06 -0500

    add arg for installation

--- modules/cmd_args.py
+++ modules/cmd_args.py
@@ -119,3 +119,4 @@ parser.add_argument('--timeout-keep-alive', type=int, default=30, help='set time
 parser.add_argument("--disable-all-extensions", action='store_true', help="prevent all extensions from running regardless of any other settings", default=False)
 parser.add_argument("--disable-extra-extensions", action='store_true', help="prevent all extensions except built-in from running regardless of any other settings", default=False)
 parser.add_argument("--skip-load-model-at-start", action='store_true', help="if load a model at web start, only take effect when --nowebui", )
+parser.add_argument("--install-only", action='store_true', help='initialize environment and install dependencies without running the API or webui", default=False)
